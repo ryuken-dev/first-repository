ef42737f6794809244dcba4dad53fa53086c4975
Tuấn Khánh <bdstuankhanh@gmail.com>
2023-10-30 11:37:51 +0700

    first commit

---
+++ src/HelloWorld.php
@@ -0,0 +1,11 @@
+<?php
+
+    namespace ryuken\FirstRepository\HelloWorld;
+
+    class HelloWorld
+    {
+        public static function world()
+        {
+            return 'Hello World, Composer!';
+        }
+    }
